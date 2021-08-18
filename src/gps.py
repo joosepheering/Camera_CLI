bba32823ba5e3c1bc079b9946b31c66c85b89eb4
Joosep Heering <joosep.heering@ttu.ee>
2021-08-18 13:38:15 +0300

    GPS random added

--- src/gps.py
+++ src/gps.py
@@ -14,7 +14,7 @@ class GPS:
         self.lat = 58.0
         self.lon = 25.0
         self.alt = 15.0
-        self.digit = 0.0
+        self.digit = random.uniform(0, 1)
         pass
 
     def __connect_to_gps(self):
@@ -25,7 +25,6 @@ class GPS:
         Get coordinates from GPS, parse it to dict.
         :return: {"lat": -24.0231, "lon": 0.22132}
         """
-        self.digit += 0.1
         lat = float(self.lat + self.digit)
         lon = float(self.lon + self.digit)
         alt = float(self.alt + self.digit)
