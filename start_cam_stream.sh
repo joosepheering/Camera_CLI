42b34fde8f648d4c84b3f2cbff98144b7bbc1d1d
Joosep Heering <joosep.heering@ttu.ee>
2021-08-20 11:53:44 +0300

    First commit

---
+++ start_cam_stream.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+while getopts "g:p:l:t:" opt
+do
+  case "${opt}" in
+    g) gpspath="${OPTARG}";;
+    p) projectid="${OPTARG}";;
+    l) pwrlinename="${OPTARG}";;
+    t) token="${OPTARG}";;
+  esac
+done
+
+echo "$gpspath";
+echo "$projectid"
+echo "$pwrlinename"
+echo "$token"
+
+cd /home/pi/Camera_CLI
+python3 camera.py -g $gpspath -p $projectid -l $pwrlinename -t $token
